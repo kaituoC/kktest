8ebdf3f543fb16c26b0c3e4473b5045f7c5d638d
kaituo <changktforever@163.com>
2018-08-29 12:13:18 +0800

    init project

---
+++ src/main/java/vip/dreamaker/kktest/KktestApplication.java
@@ -0,0 +1,12 @@
+package vip.dreamaker.kktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KktestApplication.class, args);
+    }
+}
---
+++ src/main/java/vip/dreamaker/kktest/controller/TestController.java
@@ -0,0 +1,37 @@
+package vip.dreamaker.kktest.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.stream.Collectors;
+
+/**
+ * Description:
+ * User: kaituo
+ * Date: 2018-08-29
+ * Time: 11:25
+ */
+@RestController
+@RequestMapping(value = "kktest")
+public class TestController {
+    @RequestMapping(value = "/test")
+    public String test(HttpServletRequest request, @RequestBody String inputStream) throws IOException {
+        String data = request.getParameter("data");
+        System.out.println("parameter data = " + data);
+//        String inputStream = "";
+//        try {
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+//            inputStream = bufferedReader.lines().collect(Collectors.joining(System.lineSeparator()));
+//        } catch (Exception e) {
+//            System.out.println("ERROR: " + e.getMessage());
+//        }
+        System.out.println("inputStream = " + inputStream);
+        String result = data + " ; " + inputStream;
+        System.out.println(result);
+        return result;
+    }
+}
