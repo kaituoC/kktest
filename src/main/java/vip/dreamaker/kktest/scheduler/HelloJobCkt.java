e3ab10778021748a96cbf06971482abdd8f4f4f5
kaituo <changkaituo@qknode.com>
2019-11-14 16:57:28 +0800

    + 调整xxl-job执行输出日志

--- src/main/java/vip/dreamaker/kktest/scheduler/HelloJobCkt.java
+++ src/main/java/vip/dreamaker/kktest/scheduler/HelloJobCkt.java
@@ -5,25 +5,31 @@ import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
 import com.xxl.job.core.log.XxlJobLogger;
 import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
  * @author kaituo
  * @date 2019/11/14 15:46
  */
+@Slf4j
 @JobHandler(value = "helloJobCkt")
 @Component
 public class HelloJobCkt extends IJobHandler {
 
   @Override
-  public ReturnT<String> execute(String s) throws Exception {
-    XxlJobLogger.log("helloJobCkt, Hello!");
+  public ReturnT<String> execute(String param) throws Exception {
+    XxlJobLogger.log("helloJobCkt, Hello!, param:" + param);
+    log.info("helloJobCkt, Hello!, param:{}", param);
 
     for (int i = 0; i < 5; i++) {
       XxlJobLogger.log("beat at:" + i);
       TimeUnit.SECONDS.sleep(3);
     }
-
-    return SUCCESS;
+    ReturnT<String> result = new ReturnT<>();
+    result.setCode(ReturnT.SUCCESS_CODE);
+    result.setMsg("ok");
+    log.info("result:{}", result.toString());
+    return result;
   }
 }
